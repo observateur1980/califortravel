4294d18568c2c5c11eda40888f484828dc47b153
observateur1980 <44187905+observateur1980@users.noreply.github.com>
2019-05-12 17:58:56 -0700

    12 may 5:58

---
+++ static/js/plugins - Copy.js
@@ -0,0 +1,136 @@
+
+/**
+
+	jQuery Plugins that included in this files are as followings:
+	
+	1. spin.min.js
+	2. jquery.introLoader.min.js
+	3. SmoothScroll.min.js
+	4. jquery.easing.1.3.js
+	5. jquery.waypoints.min.js
+	6. wow.min.js
+	7. fancySelect.js
+	8. jquery.slicknav.min.js
+	9. jquery.matchHeight-min.js
+	10. jquery.placeholder.min.js
+	11. slick.min.js
+	12. jquery.raty.js
+	13. jquery.filer.min.js
+	14. bootstrap-tabcollapse.js
+	15. ion.rangeSlider.min.js
+	16. moment.min.js
+	17. readmore.min.js
+	18. instagram.min.js
+	19. jquery.stickit.js
+	20. jquery.typeahead.min.js
+	21. jquery.bootstrap-touchspin.min.js
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
